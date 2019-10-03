a2831aac61d387e22d4902989cbd7651657645f7
bigyo-fourdesire <bigyo.tseng@fourdesire.com>
2019-10-03 09:30:38 +0800

    Add problem 13 folder and template

---
+++ 13/13.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str):
+
+        return 0
+
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+s = Solution()
+assert s.romanToInt("I") == 1
