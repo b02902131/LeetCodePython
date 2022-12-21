b779ef6cbe7aa6e5cfececd4e9e6b2ed01603767
bigyo-fourdesire <bigyo.tseng@fourdesire.com>
2022-12-21 16:53:42 +0800

    Add 1480.cs

--- 1480/1480.cs
+++ 1480/1480.cs
@@ -1,6 +1,19 @@
-public class Solution {
-    public int[] RunningSum(int[] nums) {
-        int[] outputNums = new int [nums.length]
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        // try-1
+        // int[] outputNums = new int [nums.Length];
+        // outputNums[0] = nums[0];
+        // for(int i = 1; i < outputNums.Length ; i++) {
+        //     outputNums[i] = outputNums[i-1] + nums[i];
+        // }
+        // return outputNums;
+        // try-2
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            nums[i + 1] += nums[i];
+        }
+        return nums;
     }
 }
-
